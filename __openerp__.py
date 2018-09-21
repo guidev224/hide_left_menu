d3a24f037618a7fd4b8fba206e9b9c2f0f673f9d
guidev224 <guidev224@gmail.com>
2018-09-21 15:43:02 +0100

    Initial commit

---
+++ __openerp__.py
@@ -0,0 +1,16 @@
+{
+    'name' : 'Hide Odoo 8 Left Bar',
+    'version' : '0.1',
+    'author' : 'BAS',
+    'category' : 'Website',
+    'summary': 'Add a button the top-left of the horizontal navigation bar to give you the ability to hide/show the left menu to gain more space.',
+    'website' : '',
+    'description': """
+
+    """,
+    'depends' : ['base'],
+    'data':[
+        'views.xml',
+        ],
+    'installable': True
+}
